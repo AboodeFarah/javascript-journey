c3c471a7bc29dceff297d7ea0652e0e789eac1ff
AboodeFarah <aboodefarah7@gmail.comcl>
2026-02-12 23:53:36 +0300

    added data type

--- JavaScript Basics/Variables and Data Types/main.js
+++ JavaScript Basics/Variables and Data Types/main.js
@@ -1,6 +1,33 @@
+// variable
+
 var name = "aboode farah"
-let city = "mogadishu"
-const age = "12"
 console.log(name);
+
+let city = "mogadishu"
 console.log(city);
+
+const age = "12"
 console.log(age);
+
+
+// data types
+
+
+// - **String:** Text wrapped in quotes.   
+
+    let greeting = "Hello, World!";
+    console.log(greeting)
+    
+    
+// - **Number:** Numeric values.
+
+    let year = 2024;
+    console.log(year)
+    
+
+    
+// - **Boolean:** True or false values.
+
+       let isJavaScriptFun = true;
+       console.log(isJavaScriptFun)
+   
