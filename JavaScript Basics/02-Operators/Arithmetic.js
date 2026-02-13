e44b46d9411056db3bfd21dbc2f3de9665797b47
AboodeFarah <aboodefarah7@gmail.comcl>
2026-02-13 16:02:29 +0300

    complate Arithmetic lesson

--- JavaScript Basics/02-Operators/Arithmetic.js
+++ JavaScript Basics/02-Operators/Arithmetic.js
@@ -1,4 +1,40 @@
-let y = 5;
+
+// Arithemetics
 let x = 10;
-let result = x+y;
-console.log(result);
+let y = 5;
+
+// Add +
+console.log(x + y);
+
+// Subtraction -
+console.log(x - y);
+
+// Multiplication *
+console.log(x * y);
+
+// Division /
+console.log(x / y);
+
+// Modulus %
+console.log(x % y);
+
+// Post increment ++
+// Marka hore ayuu isticmaalaa value-ga hore, Kadib ayuu kordhiyaa
+// The result of post-increment is 10 because it prints the old value first,
+// then increases the value by 1.
+// So after this line runs, the value becomes 11.
+console.log(x++);
+
+// result of post now is 11
+console.log(x)
+
+// pre increment
+// Marka hore ayuu kordhiyaa value-ga, Kadib ayuu isticmaalaa value-ga cusub
+// The result of pre-increment is now 12 because post-increment prints the old value first and then adds 1.
+// So after post-increment, the value became 11. With pre-increment, the result becomes 12.
+console.log(++x);
+
+// Decrement --
+// is the same idea of increment
+console.log(x--);
+console.log(--x)
