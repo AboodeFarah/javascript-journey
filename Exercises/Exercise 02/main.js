3185034cf861345571244f53307a8c7854c699b4
AboodeFarah <aboodefarah7@gmail.com>
2026-02-14 16:25:41 +0300

    ex 2

--- Exercises/Exercise 02/main.js
+++ Exercises/Exercise 02/main.js
@@ -1,1 +1,28 @@
-// hi yow selayhy
+//Declare two variables a with a value of 15 and b with a value of "15". Use all comparison operators and log the results.
+
+let a = 15;
+let b = "15";
+
+//equal to
+console.log(a == b); //true
+
+// //not equal to 
+console.log(a != b); //false 
+
+// // Strictly equal to 
+console.log(a === b); //false 
+
+// // Strictly  not equal to
+console.log(a !== b); //true
+
+// //greater than
+console.log(a > b); //false
+
+// //less than
+console.log(a < b); //false
+
+// // Greater than or equal to 
+console.log(a >= b); //true
+
+// // less than or equal to 
+console.log(a <= b); //true
