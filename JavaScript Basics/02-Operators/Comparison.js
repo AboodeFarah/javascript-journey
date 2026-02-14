9256f58fcd32ba8be7f4b73aef16812058b66ccf
AboodeFarah <aboodefarah7@gmail.com>
2026-02-14 15:56:52 +0300

    complate comparison lesson

---
+++ JavaScript Basics/02-Operators/Comparison.js
@@ -0,0 +1,27 @@
+
+let x = 10 ;
+let y = "10" ;
+
+// equal to 
+console.log(x == y)   //true
+
+// not equal to
+console.log(x != y)  //false
+
+// strictly equal to 
+console.log(x === y);   //false 
+
+// strictly not equal to 
+console.log(x !== y);   //true
+
+//greater than
+console.log(x > y);
+
+//less than
+console.log(x < y);
+
+//greater than or equal
+console.log(x >= y);
+
+// less than or equal
+console.log(x <= y);
