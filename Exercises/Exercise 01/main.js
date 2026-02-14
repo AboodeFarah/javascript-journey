0ea6f14cf2baf3bf3b5a7c1995efa6a744c53faa
AboodeFarah <aboodefarah7@gmail.com>
2026-02-14 13:01:26 +0300

    add ex one

---
+++ Exercises/Exercise 01/main.js
@@ -0,0 +1,13 @@
+// Create a variable x with a value of 7 and a variable y with a value of 3. Perform all the arithmetic operations and log the results.
+
+let x = 7;
+let y = 3;
+console.log(x + y);  
+console.log(x - y);  
+console.log(x * y);  
+console.log(x / y);  
+console.log(x % y);  
+console.log(x++);    
+console.log(x--);   
+
+// to see the result cd "Exercise"/ and cd "Exercise\ 01"/ then open the terminal and use node main.js
