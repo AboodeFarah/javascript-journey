cd1001ebe0f66703fe03d59cd7739479756e1b3f
AboodeFarah <aboodefarah7@gmail.com>
2026-02-15 15:43:19 +0300

    complate Exercise 03

---
+++ Exercises/Exercise 03/main.js
@@ -0,0 +1,6 @@
+// Create two boolean variables isSunny and isWeekend. Use logical operators to determine if you can go to the beach (both need to be true) and log the results.
+
+let isSunny = true;
+let isWeekend = true;
+
+console.log("can you go to the beach?", isSunny && isWeekend);
