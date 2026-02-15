15983df4f4178099d4d4bfb0c953a7c5606af975
AboodeFarah <aboodefarah7@gmail.com>
2026-02-15 15:25:16 +0300

    added examples

---
+++ JavaScript Basics/02-Operators/Logical.js
@@ -0,0 +1,14 @@
+
+
+let isStudent = true;
+let hasPermission = false;
+
+// AND &&
+console.log(isStudent && hasPermission);  //false
+
+// OR ||
+console.log(isStudent || hasPermission);  //true
+
+// Inver !
+console.log(!isStudent);  //false
+console.log(!hasPermission);  //true
