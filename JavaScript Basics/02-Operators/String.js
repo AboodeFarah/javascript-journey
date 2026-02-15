800ea6e93b467a6235abc110d9446e7c67ad068e
AboodeFarah <aboodefarah7@gmail.com>
2026-02-15 16:23:07 +0300

    added new lesson about string concatenate

---
+++ JavaScript Basics/02-Operators/String.js
@@ -0,0 +1,15 @@
+
+//concatenate
+let firstName = "aboode ";
+let lastName = "faarah";
+
+console.log(firstName+ " " +lastName);
+
+
+//concatenate assignment
+let greeting = "Hi,"
+greeting += firstName ;
+greeting += lastName ;
+greeting += " welcome to the word"
+
+console.log(greeting);
