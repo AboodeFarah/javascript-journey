477b247df6d4ee31e94e9e6a17b048a671965db8
AboodeFarah <aboodefarah7@gmail.com>
2026-02-16 15:15:12 +0300

    added function declaration examples

---
+++ Javascript Functions/Different Types of Functions/01 Function Declaration.js
@@ -0,0 +1,13 @@
+// Syntax
+// function functionName(parameters) {
+//     code to be executed
+// }
+// functionName()
+
+
+//examples:
+function name() {
+    console.log("hello aboode farah");
+}
+name()
+
