fa5eee9e6d444a9b7358383bd0496a18ab1668bc
AboodeFarah <aboodefarah7@gmail.com>
2026-02-16 15:47:27 +0300

    added Parameters and Arguments examples

--- Javascript Functions/Different Types of Functions/01 Function Declaration.js
+++ Javascript Functions/Different Types of Functions/01 Function Declaration.js
@@ -6,8 +6,15 @@
 
 
 //examples:
-function name() {
-    console.log("hello aboode farah");
-}
-name()
 
+// function name() {
+//     console.log("hello aboode farah");
+// }
+// name()
+
+
+// using Parameters and Arguments
+function greeting(name){   //parameters is name "is like variable inside function"
+    console.log("hello " + name)  //here we conect the string with variable 
+}
+greeting("aboode farah")  //argument is the value of finction parameters
