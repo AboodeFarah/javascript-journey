4b07ca19fb3fea467e3802c80f0b3ad55587e1f0
AboodeFarah <aboodefarah7@gmail.com>
2026-02-16 22:20:51 +0300

    complate exercise 05

---
+++ Exercises/Exercise 05/main.js
@@ -0,0 +1,13 @@
+// 3. **Arrow Functions:**
+    
+//     Convert the `add` function to an arrow function and call it with different arguments.
+
+const add = (a,b) => {
+    return a +b ;
+}
+let result = add(83,86);
+let result2 = add(637,583);
+let result3 = add(37,8);
+console.log(result);
+console.log(result2);
+console.log(result3);
