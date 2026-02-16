13ab8688ef94d1e143dc613cb59f38447aea6cac
AboodeFarah <aboodefarah7@gmail.com>
2026-02-16 18:50:29 +0300

    added function expression example

---
+++ Javascript Functions/Different Types of Functions/02 Function Expression.js
@@ -0,0 +1,14 @@
+//function expression
+
+//the only difference between them is the syntax
+let greeting = function(name){
+    console.log("hello" + name);
+}
+greeting(" aboode")
+
+//function expression Add
+const add = function(a,b){
+    return a+b;
+}
+let result = add(10,10);
+console.log(result);
