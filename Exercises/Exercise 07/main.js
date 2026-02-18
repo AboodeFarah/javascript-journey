db208256fa5ab027d345df08a27844f4f6a21211
AboodeFarah <aboodefarah7@gmail.com>
2026-02-18 11:33:50 +0300

    complate exercise 07

---
+++ Exercises/Exercise 07/main.js
@@ -0,0 +1,10 @@
+// Add a color to the end of the colors array, remove the first color, and log the array.
+
+let colors = ["red", "green", "blue"];
+colors.push("yellow");
+console.log(colors)
+
+colors.shift();
+console.log(colors);
+
+console.log(colors.length);
