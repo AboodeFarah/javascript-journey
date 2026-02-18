4bc30b4221c5775ef06c56551d06d0df61ef784f
AboodeFarah <aboodefarah7@gmail.com>
2026-02-18 11:08:54 +0300

    added array method lesson

--- Arrays and Objects/Arrays/Arrays.js
+++ Arrays and Objects/Arrays/Arrays.js
@@ -3,3 +3,19 @@
 let car = ["toyota", "ford","BMW"];
 console.log(car[0])
 console.log(car[2])
+
+
+// common array method
+
+let color = ["red", "blue", "yellow"]
+color.push("green")  //add an element to the end of array
+console.log(color);
+
+color.pop()     //remove the last element of the array
+console.log(color);
+
+color.shift();  //remove the first element of the array
+console.log(color);
+
+color.unshift("purple") //add elements to the begining of the array
+console.log(color);
