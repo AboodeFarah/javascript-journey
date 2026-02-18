04e83f37f257d007b829dd6c2d1cfc08a46e206b
AboodeFarah <aboodefarah7@gmail.com>
2026-02-18 13:22:29 +0300

    Added Object lesson

---
+++ Arrays and Objects/Objects/Objects.js
@@ -0,0 +1,17 @@
+// Object
+
+let person = {
+    name : "aboode",
+    age : 23,
+    city : "ceelbuur",
+    bloodType : "AB+"
+}
+console.log(person.name)
+console.log(person.age)
+console.log(person.city)
+console.log(person.bloodType)
+
+
+// array ways
+
+console.log(person["name"])
