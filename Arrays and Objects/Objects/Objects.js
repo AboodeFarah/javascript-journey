582d5b1b9a926968ccd2bf69b7cf4625b576d8c7
AboodeFarah <aboodefarah7@gmail.com>
2026-02-20 13:19:07 +0300

    adding and modifying of properties on object lesson

--- Arrays and Objects/Objects/Objects.js
+++ Arrays and Objects/Objects/Objects.js
@@ -6,12 +6,28 @@ let person = {
     city : "ceelbuur",
     bloodType : "AB+"
 }
-console.log(person.name)
-console.log(person.age)
-console.log(person.city)
-console.log(person.bloodType)
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.city)
+// console.log(person.bloodType)
 
 
 // array ways
 
 console.log(person["name"])
+
+
+// adding and modifying properties
+
+let person2 = {
+    name : "aboode",
+    age : 23,
+    city : "ceelbuur",
+    
+}
+
+person2.birthDay = 2002-2-16;  // Adding a new property
+console.log(person2);         //output  name:'aboode',age:23,city:'ceelbuur',birthDay: 1984
+
+person2.age = 30;   // Modifying an existing property
+console.log(person2);   //output  name: 'aboode', age: 30,city: 'ceelbuur', birthDay: 1984
