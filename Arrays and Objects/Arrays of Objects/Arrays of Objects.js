a5eac7917b18fe407dadc7ac05d74e6314a16c53
AboodeFarah <aboodefarah7@gmail.com>
2026-02-20 14:12:22 +0300

    added  Arrays of Objects lesson

---
+++ Arrays and Objects/Arrays of Objects/Arrays of Objects.js
@@ -0,0 +1,40 @@
+
+
+let students = [
+    {
+        name : " aboode",
+        class : "CMSA5",
+        Id : 133250,
+        age : 23,
+        city : "ceelbuur",
+    },
+
+    {
+        name : " faarax",
+        class : "CMSA3",
+        Id : 13229,
+        age : 29,
+        city : "mogadisho",
+    },
+
+    {
+        name : " abdalla",
+        class : "CMSB2",
+        Id : 135467,
+        age : 25,
+        city : "laascaanood",
+    }
+    
+];
+
+console.log(students[0].name)
+console.log(students[1].name)
+console.log(students[2].name)
+
+console.log(students[0].Id)
+console.log(students[1].Id)
+console.log(students[2].Id)
+
+console.log(students[0].city)
+console.log(students[1].city)
+console.log(students[2].city)
