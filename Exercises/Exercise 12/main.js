df30f2e2ae53762809592bc9ca83f430aa8085eb
AboodeFarah <aboodefarah7@gmail.com>
2026-02-23 12:56:49 +0300

    complate exercise 12

---
+++ Exercises/Exercise 12/main.js
@@ -0,0 +1,9 @@
+// Write a for loop that logs each item in the array ["apple", "banana", "cherry"].
+
+const vagitables = ["apple", "banana", "cherry", "orange"]
+
+for(let i = 0; i < vagitables.length; i++){
+
+    console.log(vagitables[i])
+
+}
