2f8071b676941b28d5c61f13301b893dddd122af
AboodeFarah <aboodefarah7@gmail.com>
2026-02-23 12:34:41 +0300

    added another example

--- Loops/For Loop/forLoop.js
+++ Loops/For Loop/forLoop.js
@@ -1,8 +1,8 @@
 // for loop
 
-// for( let x = 0; x < 10; x++){
-//     console.log("hello");
-// }
+for( let x = 0; x < 10; x++){
+    console.log("hello");
+}
 
 // how to Work
 // let i = 0 → starting point
