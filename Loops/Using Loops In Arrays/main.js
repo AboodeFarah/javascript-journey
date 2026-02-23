6b4626525128cc573573f9004f248245430cbe84
AboodeFarah <aboodefarah7@gmail.com>
2026-02-23 13:04:37 +0300

    using big data real example

---
+++ Loops/Using Loops In Arrays/main.js
@@ -0,0 +1,46 @@
+// Meaningful Example:
+
+let fruits = [
+  "Apple",
+  "Banana",
+  "Orange",
+  "Mango",
+  "Pineapple",
+  "Strawberry",
+  "Blueberry",
+  "Raspberry",
+  "Blackberry",
+  "Grapes",
+  "Watermelon",
+  "Cantaloupe",
+  "Papaya",
+  "Kiwi",
+  "Peach",
+  "Pear",
+  "Plum",
+  "Cherry",
+  "Apricot",
+  "Pomegranate",
+  "Guava",
+  "Lemon",
+  "Lime",
+  "Coconut",
+  "Avocado",
+  "Fig",
+  "Date",
+  "Passionfruit",
+  "Dragonfruit",
+  "Lychee",
+  "Jackfruit",
+  "Tangerine",
+  "Cranberry",
+  "Persimmon",
+  "Starfruit",
+  "Grapefruit",
+  "Mulberry",
+  "Nectarine"
+];
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i])
+}
