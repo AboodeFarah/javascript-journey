c04a32700e7f8ebe564cfbe138b38cfeebfdef26
AboodeFarah <aboodefarah7@gmail.com>
2026-02-24 10:31:40 +0300

    complate Exercise 14

---
+++ Exercises/Exercise 14/main.js
@@ -0,0 +1,11 @@
+// Write a do...while loop that asks the user to enter a number greater than 10 and repeats until they do so.
+
+let number;
+
+do{
+
+    number = prompt("enter number greater than 10")
+
+}while(number <= 10)
+
+    console.log("wow thanks")
