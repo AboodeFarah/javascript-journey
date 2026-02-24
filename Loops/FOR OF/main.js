dcb3db8307586dc77da7dda146adf93bec8aa0c9
AboodeFarah <aboodefarah7@gmail.com>
2026-02-24 10:44:57 +0300

    Added for of loops

---
+++ Loops/FOR OF/main.js
@@ -0,0 +1,58 @@
+// for of
+
+let cars = ["car1","car2","car3"]
+
+for(let car of cars){
+    console.log("This is your car: "+ car);
+}
+
+// letter by letter
+
+const name = "Frimpi";
+for(let N of name){
+    console.log(N);
+}
+
+let fruits = [
+  "Apple",
+  "Banana",
+  "Orange",
+  "Mango",
+  "Pineapple",
+  "Strawberry",
+  "Blueberry",
+  "Raspberry",
+  "Blackberry",
+  "Grapes",
+  "Watermelon",
+  "Cantaloupe",
+  "Papaya",
+  "Kiwi",
+  "Peach",
+  "Pear",
+  "Plum",
+  "Cherry",
+  "Apricot",
+  "Pomegranate",
+  "Guava",
+  "Lemon",
+  "Lime",
+  "Coconut",
+  "Avocado",
+  "Fig",
+  "Date",
+  "Passionfruit",
+  "Dragonfruit",
+  "Lychee",
+  "Jackfruit",
+  "Tangerine",
+  "Cranberry",
+  "Persimmon",
+  "Starfruit",
+  "Grapefruit",
+  "Mulberry",
+  "Nectarine"
+];
+for(let fruit of fruits){
+    console.log("This fruit call: "+fruit)
+}
