f5513f8626181cf3c108b1033ba3e151df5478c5
AboodeFarah <aboodefarah7@gmail.com>
2026-02-24 10:55:17 +0300

    complate for in examples

---
+++ Loops/FOR IN/main.js
@@ -0,0 +1,12 @@
+// for in 
+
+let person = {
+    name : "aboode",
+    age : 23,
+    city : "ceelbuur",
+    bloodType : "AB+"
+}
+
+for(let key in person){
+    console.log("the infomis: "+ person[key])
+}
