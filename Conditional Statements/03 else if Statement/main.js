e3a1e6c1634e26159894acfaf2246eacb644ae9c
AboodeFarah <aboodefarah7@gmail.com>
2026-02-27 17:28:12 +0300

    added new lesson

---
+++ Conditional Statements/03 else if Statement/main.js
@@ -0,0 +1,14 @@
+// else if 
+// Used when checking multiple conditions.
+
+let age = 33;
+
+if(age < 18){
+    console.log("you are child.")
+}
+else if(age < 20){
+    console.log("you are teenager.")
+}
+else{
+    console.log('you are an adult')
+}
