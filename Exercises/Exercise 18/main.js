f2b3927a4f1f8d58f9310b1ddf4db810be9c32f9
AboodeFarah <aboodefarah7@gmail.com>
2026-02-27 18:00:24 +0300

    complate exercise 18

---
+++ Exercises/Exercise 18/main.js
@@ -0,0 +1,6 @@
+// Use the ternary operator to log "Pass" if the variable grade is 60 or more, and "Fail" otherwise.
+
+const grade = 20;
+
+const message = grade >= 60? "Pass" : "Fail";
+console.log(message)
