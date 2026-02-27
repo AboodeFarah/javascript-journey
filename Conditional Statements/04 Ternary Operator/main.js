e24c1d378a388efcdccb52f1b6e3715987fcb0b6
AboodeFarah <aboodefarah7@gmail.com>
2026-02-27 17:56:01 +0300

    added Ternary Operator lessons

---
+++ Conditional Statements/04 Ternary Operator/main.js
@@ -0,0 +1,16 @@
+// Ternary Operator
+// Short version of if...else.
+// condition ? expressionIfTrue : expressionIfFalse
+
+
+// if..else 
+let age = 10;
+
+let message = age >= 18? "you are an adult" : "ciyaal";
+
+console.log(message)
+
+
+// if
+const me = age >= 10 && "hi"
+console.log(me)
