b839e76e84bc70f54c3d0a165350593096796943
AboodeFarah <aboodefarah7@gmail.com>
2026-02-27 17:36:59 +0300

    complate exercise 17

---
+++ Exercises/Exercise 17/main.js
@@ -0,0 +1,20 @@
+// Write an `else if` statement that logs:
+//     - "Very cold" if the temperature is below 0,
+//     - "Cold" if the temperature is between 0 and 15,
+//     - "Warm" if the temperature is between 15 and 25,
+//     - "Hot" if the temperature is above 25.
+
+let temperature = 35;
+
+if(temperature < 0){
+    console.log("very cold");
+}
+else if(temperature < 15){
+    console.log("Cold")
+}
+else if(temperature < 25 ){
+    console.log("Warm")
+}
+else{
+    console.log("Hot")
+}
