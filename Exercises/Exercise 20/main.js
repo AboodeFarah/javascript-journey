2f537478721d84a3628456b15220b1679e82d8bb
AboodeFarah <aboodefarah7@gmail.com>
2026-03-04 23:28:11 +0300

    complate exercise 20

---
+++ Exercises/Exercise 20/main.js
@@ -0,0 +1,7 @@
+// Use the map() method to create a new array that contains the lengths of each string in the array ["apple", "banana", "cherry"].
+
+
+const fruits = ["apple", "banana", "cherry"];
+
+const newFruits = fruits.map((fruit) => fruit)
+    console.log(newFruits)
