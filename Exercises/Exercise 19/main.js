8650ce0ea6d0ba647ec9cd3faab6f0dfacbd92a9
AboodeFarah <aboodefarah7@gmail.com>
2026-03-04 23:07:37 +0300

    complate exercise 19

---
+++ Exercises/Exercise 19/main.js
@@ -0,0 +1,7 @@
+// Use the forEach() method to log each number in the array [1, 2, 3, 4, 5] to the console.
+
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number) => {
+    console.log(number)
+})
