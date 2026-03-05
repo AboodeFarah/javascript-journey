d0cf1d98a0d6463d7fe6633450119f3e01ef6ba0
AboodeFarah <aboodefarah7@gmail.com>
2026-03-05 16:00:37 +0300

    complate exercise 23

---
+++ Exercises/Exercise 23/main.js
@@ -0,0 +1,5 @@
+// Use object destructuring to extract the make and model properties from the object { make: "Toyota", model: "Corolla", year: 2020 }.
+
+const car = { make: "Toyota", model: "Corolla", year: 2020 }
+const {make,model} = car;
+console.log(make,model)
