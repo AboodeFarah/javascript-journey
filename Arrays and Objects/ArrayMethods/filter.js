9140943ef0d8ead4de1743ba71c81686a067a7a8
AboodeFarah <aboodefarah7@gmail.com>
2026-03-05 11:32:34 +0300

    add filter lesson

---
+++ Arrays and Objects/ArrayMethods/filter.js
@@ -0,0 +1,15 @@
+// filters 
+
+// let numbera = [1,2,3,4,5,6,7,8,9,10]
+
+// const evenNumber = numbera.filter((num) => {
+//     return num % 2 === 0
+// })
+// console.log(evenNumber)
+
+let numbers = [10, 15, 20, 25, 30];
+
+const even = numbers.filter((num) => {
+    return num %2===0;
+})
+console.log(even)
