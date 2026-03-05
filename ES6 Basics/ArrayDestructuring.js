3c07caa4d9b179650b4f369ef444e1093c58e0e7
AboodeFarah <aboodefarah7@gmail.com>
2026-03-05 15:30:30 +0300

    added array Destructuring lesson

---
+++ ES6 Basics/ArrayDestructuring.js
@@ -0,0 +1,14 @@
+// array Destructuring
+
+const fruits = ["banaana","apple","avogado"];
+
+const [first,second,third] = fruits;
+console.log(third)
+
+// old wey 
+// const banaana = fruits[0]
+// const apple = fruits[1]
+// const avogado = fruits[2]
+// console.log(banaana)
+// console.log(apple)
+// console.log(avogado)
