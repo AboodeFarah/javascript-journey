e7cedcbdb4ef1f14b404088ec15e8089501740fd
AboodeFarah <aboodefarah7@gmail.com>
2026-03-05 15:01:23 +0300

    new lesson

--- ES6 Basics/TemplateLiterals.js
+++ ES6 Basics/TemplateLiterals.js
@@ -1,4 +1,6 @@
 let name = "aboode";
 let city = "mogadishu";
 let message = `Hello i'm ${name} hope you feel good i'm from ${city} and you?`
+// old wey 
+// let message = "Hello i'm " +name+ " hope you feel good i'm from " +city+ " and you?"
 console.log(message)
