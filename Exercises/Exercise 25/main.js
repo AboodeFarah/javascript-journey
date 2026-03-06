032643c6c64ca37b0a141c9ee35ed70fa625fb33
AboodeFarah <aboodefarah7@gmail.com>
2026-03-06 14:04:39 +0300

    new exercise

---
+++ Exercises/Exercise 25/main.js
@@ -0,0 +1,3 @@
+// 1. Use the spread operator to combine two arrays `[1, 2, 3]` and `[4, 5, 6]` into a single array.
+// 2. Create a function `multiply` that takes any number of arguments and returns their product using the rest parameter.
+
