0bc429693b73301a6ced041033a2712c5ad2f42c
AboodeFarah <aboodefarah7@gmail.com>
2026-03-07 15:09:03 +0300

    new lesson

--- Asynchronous Programming/Asynchronous Non-Blocking.js
+++ Asynchronous Programming/Asynchronous Non-Blocking.js
@@ -1,2 +1,18 @@
 // Asynchronous Non-Blocking
 
+function getUserData (callback){
+    setTimeout(() => {
+        const user = {
+            name : "abdalla",
+            id : 1
+        }
+        callback(user);
+    },5000);
+}
+console.log("starting fetching user data")
+
+getUserData(function(user){
+    console.log(user)
+})
+
+console.log("this userdata shows immediately")
