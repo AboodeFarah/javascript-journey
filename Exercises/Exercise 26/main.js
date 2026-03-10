bfd7a41068cfdc91711cf54b860105a604eb15c0
AboodeFarah <aboodefarah7@gmail.com>
2026-03-10 15:02:18 +0300

    new exercise

---
+++ Exercises/Exercise 26/main.js
@@ -0,0 +1,2 @@
+// Task: Implement a simple blocking function that simulates a delay of 2 seconds before returning a message. Then, implement a non-blocking version of the same function using setTimeout.
+
