6e84175c725ecbe0f53dce7c592a39bfaa416c5a
AboodeFarah <aboodefarah7@gmail.com>
2026-03-11 14:15:11 +0300

    new lesson

--- Asynchronous Programming/Promises.js
+++ Asynchronous Programming/Promises.js
@@ -1,2 +1,20 @@
 // Promise
 
+function fetchUserData (){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+            const secuss = true;
+            if(secuss=== true){
+                resolve({name : "abdalla",age : 30,id : 1})
+            }else{
+                reject("error fetching user data")
+            }
+
+        }, 3000);
+    });
+
+}
+fetchUserData()
+.then((userData)=> console.log("data fetched successfully",userData))
+.catch((error)=>console.log("error fetching data",error));
