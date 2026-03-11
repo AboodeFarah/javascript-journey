7833b4e335904f88c4d63f076c3dfd9d42ce4fdd
AboodeFarah <aboodefarah7@gmail.com>
2026-03-11 15:13:23 +0300

    complate exercise 28

---
+++ Exercises/Exercise 28/main.js
@@ -0,0 +1,24 @@
+// Task: Rewrite the function from Exercise 2 to use async and await instead of .then() and .catch(). Ensure that the success message is logged only after the data is fetched.
+
+function delayedSuccess() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const success = false; // Simulating success condition
+            if (success) {
+                resolve("Operation completed successfully!");
+            } else {
+                reject("An error occurred during the operation.");
+            }
+        }, 3000);
+    });
+}
+
+async function displayUserData () {
+    try{
+        const data = await delayedSuccess();
+        console.log(data);
+    }catch(error){
+        console.log(error)
+    }
+}
+displayUserData()
