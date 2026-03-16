496dbc1bae54bdb5c437ba7937cf64e5b9ade6dc
AboodeFarah <aboodefarah7@gmail.com>
2026-03-16 15:29:06 +0300

    new lesson

--- Asynchronous Programming/callBack.js
+++ Asynchronous Programming/callBack.js
@@ -1,10 +1,29 @@
 
 
-function greet(name){
-    console.log("Hello ", name);
+// function greet(name){
+//     console.log("Hello ", name);
+// }
+// function processUserInput(callback){
+//     const message = prompt("Please enter your name.");
+//     callback(message);
+// }
+// processUserInput(greet);
+
+
+function operate(num1, num2, callback){
+    return callback(num1, num2);
 }
-function processUserInput(callback){
-    const message = prompt("Please enter your name.");
-    callback(message);
+
+function add(a, b){
+    return a + b;
 }
-processUserInput(greet);
+function multiply(a, b){
+    return a * b;
+}
+function subtract(a, b){
+    return a - b;
+}
+
+console.log("addition:", operate(2, 3, add));
+console.log("multiplication:", operate(2, 3, multiply));
+console.log("subtraction:", operate(10, 5, subtract));
