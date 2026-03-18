c11ae11c727db91542c1bf1c82355dab85bebb78
AboodeFarah <aboodefarah7@gmail.com>
2026-03-18 03:17:52 +0300

    complate exercise 31

--- Exercises/Exercise 31/main.js
+++ Exercises/Exercise 31/main.js
@@ -12,7 +12,7 @@ async function fetchUsers() {
 
     try{
 
-        const response = await fetch('https://jsonplaceholder.typicode.com/userss');
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
 
         if (!response.ok) {
             throw new Error(`HTTP GET request failed with status : ${response.status}`);
